7887361f954ee5e194d0a5959420deb68d6bc5c8
kaliel <kalieleduard1@gmail.com>
2025-08-25 20:01:50 -0300

    marmelada de marmota

--- lua/lsp/lua.lua
+++ lua/lsp/lua.lua
@@ -1,6 +1,9 @@
 local lspconfig = require('lspconfig')
 
 lspconfig.lua_ls.setup {
-
+	root_dir = function()
+		-- Retorna o diretório de configuração do Neovim
+		return vim.fn.stdpath('config')
+	end,
 
 }
--- lua/lsp/terraform.lua
+++ lua/lsp/terraform.lua
@@ -1,5 +1,11 @@
 local lspconfig = require("lspconfig")
 
 lspconfig.terraformls.setup {
+	root_dir = function()
+		return vim.fn.stdpath('config')
+	end,
+}
+
+lspconfig.tflint.setup {
 
 }
